fa1282b1e532f2b501698e7a3cfc634470c26de4
Prajin Khadka <34759925+prajinkhadka@users.noreply.github.com>
2021-12-17 08:59:01 +0545

    add example

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
 def handler(event, context):
+    userID = event["userID"]
+    Token = event["Token"]
+
+    # Any logic that your app should perform.
+
     return {
         "statusCode": 200,
-        "body": "Hello there"
+        "body": "Hello there",
+        "userID" : userID,
+        "Token": Token
     }
